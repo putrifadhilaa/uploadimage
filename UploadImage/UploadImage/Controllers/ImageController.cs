b84223cd644c31b2c7a5ef4178e42e61264a5787
putri <putri.fadhilaa@gmail.com>
2018-12-05 17:02:19 +0700

    first upload, error Index.cshtml

---
+++ UploadImage/UploadImage/Controllers/ImageController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UploadImage.Models;
+
+namespace UploadImage.Controllers
+{
+    public class ImageController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Image imageModel)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+            string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            imageModel.ImagePath = "~/Image/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+            imageModel.ImageFile.SaveAs(fileName);
+            using (DbModels db = new DbModels())
+            {
+                db.Images.Add(imageModel);
+                db.SaveChanges();
+            }
+            ModelState.Clear();
+                return View();
+        }
+
+        [HttpGet]
+        public ActionResult View(int id)
+        {
+            Image imageModel = new Image();
+            using (DbModels db = new DbModels())
+            {
+                imageModel = db.Images.Where(x => x.ImageID == id).FirstOrDefault();
+            }
+            return View(imageModel);
+        }
+    }
+}
---
+++ UploadImage/UploadImage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UploadImage.Startup))]
+namespace UploadImage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
